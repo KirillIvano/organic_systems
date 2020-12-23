2f3283355f3abacf4c6bc923a1c25429ffb3a2cc
KirillIvano <ivannadez@yandex.ru>
2020-12-23 23:59:26 +0300

    update images and remove quotes carousel

--- front/src/pages/main/index.ts
+++ front/src/pages/main/index.ts
@@ -5,16 +5,6 @@ import 'owl.carousel/dist/owl.carousel';
 import '@/common';
 import '@/events';
 
-
-($('.owl-carousel.testimonials-carousel') as any).owlCarousel({
-    items: 1,
-    margin: 20,
-    stagePadding: 20,
-    autoplay: true,
-    loop: true,
-    dots: false,
-});
-
 ($('.owl-carousel.intro-carousel') as any).owlCarousel({
     items: 1,
     autoplay: true,
