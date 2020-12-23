811377854b79d99166cecd32c3c88fa2461b236c
KirillIvano <ivannadez@yandex.ru>
2020-12-23 23:57:44 +0300

    added ui events

--- front/src/events/ui/actions.ts
+++ front/src/events/ui/actions.ts
@@ -1,9 +1,19 @@
-export const TOGGLE_NAV = 'TOGGLE_NAV' as const;
+import {createAction, ActionTypes} from '../utils/types';
 
-export const actions = {
-    TOGGLE_NAV,
+
+export const OPEN_NAV = 'OPEN_NAV' as const;
+export const CLOSE_NAV = 'CLOSE_NAV' as const;
+
+export const navOpenAction = createAction(
+    OPEN_NAV,
+);
+export const navCloseAction = createAction(
+    CLOSE_NAV,
+);
+
+export const uiActions = {
+    navOpenAction,
+    navCloseAction,
 };
 
-export type UiActions = {
-    TOGGLE_NAV: () => void,
-}
+export type UiActions = ActionTypes<typeof uiActions>
